65404ec0db8cd8dec8a8f23792a3fb673a064a74
taplar <taplarhw@gmail.com>
2018-11-25 15:53:10 -0600

    Update questTracker to revision fdb0c9003bf352968656139505a56a448139e50c

---
+++ questTracker/precache-manifest.9dd87b217246cff2b93a0852b389c5b3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dee807ac3325c54cf95fdfcf45b3db8",
+    "url": "assets/styles/vendor/fontello/css/fontello-embedded.css"
+  },
+  {
+    "revision": "45ab18800a6bc3d76b69",
+    "url": "main.js"
+  },
+  {
+    "revision": "c8a8245ecaf94ceac9e93bbb9a27b6f7",
+    "url": "assets/styles/vendor/fontello/css/fontello-ie7-codes.css"
+  },
+  {
+    "revision": "f93e2e02bb97618d9d7135a3a330840b",
+    "url": "assets/styles/vendor/fontello/README.txt"
+  },
+  {
+    "revision": "64dc4203fef6ea0bb7cdc9a5ff59dd43",
+    "url": "assets/styles/vendor/fontello/font/fontello.woff2"
+  },
+  {
+    "revision": "c8ee182ad01fb0120f26c5ced0f1bb1f",
+    "url": "assets/styles/vendor/fontello/font/fontello.woff"
+  },
+  {
+    "revision": "462d90b7c7c2316a7e273798c1cbffb0",
+    "url": "assets/styles/vendor/fontello/font/fontello.ttf"
+  },
+  {
+    "revision": "e1c03416dd3eb4e85ada143962985dd5",
+    "url": "assets/styles/vendor/fontello/config.json"
+  },
+  {
+    "revision": "e7da1c1d837b0be2240c2c23bf0c4475",
+    "url": "assets/styles/vendor/fontello/css/animation.css"
+  },
+  {
+    "revision": "bb37c909d171e3612302318b09f1db5b",
+    "url": "assets/styles/vendor/fontello/css/fontello-codes.css"
+  },
+  {
+    "revision": "ff2ba0e8332fd8871bbe7ddb1666d9d6",
+    "url": "index.html"
+  },
+  {
+    "revision": "0942582a712f13f7155316fcab3c74f5",
+    "url": "assets/styles/vendor/fontello/font/fontello.svg"
+  },
+  {
+    "revision": "fee8b54e5226e9faf5d73423fde381da",
+    "url": "assets/styles/vendor/fontello/css/fontello-ie7.css"
+  },
+  {
+    "revision": "25f7a6c366fab30b5f01cdce3bea1ceb",
+    "url": "assets/styles/vendor/fontello/font/fontello.eot"
+  },
+  {
+    "revision": "109900ecf0b086b743ffcc2dd0c7d69a",
+    "url": "assets/styles/vendor/fontello/css/fontello.css"
+  },
+  {
+    "revision": "c122d88c9a08adac87e884818d126e84",
+    "url": "assets/styles/vendor/fontello/LICENSE.txt"
+  },
+  {
+    "revision": "a9eeb0f1209117a86ec111d495bf852d",
+    "url": "assets/jsons/quests.json"
+  },
+  {
+    "revision": "03aca5ccf845b612e384e7882b8191f2",
+    "url": "assets/jsons/manifest.json"
+  },
+  {
+    "revision": "c513a592603129a61a147d3be8071c07",
+    "url": "assets/images/icons/d20Icon_512x512.png"
+  },
+  {
+    "revision": "f9affdd08c00e9874ade32ad5a72ccd3",
+    "url": "assets/images/icons/d20Icon_192x192.png"
+  },
+  {
+    "revision": "f6c311c57f365b064da615589852b307",
+    "url": "assets/images/icons/d20Icon.svg"
+  }
+];
