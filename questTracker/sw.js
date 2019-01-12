401835fb5638ca8fd9a1e39f3e662e82a40702db
taplar <taplarhw@gmail.com>
2019-01-12 13:08:57 -0600

    Removed questTrackerDebug, and updated questTracker to revision 6b5f1639e0bfd0efcb5a9d1ef54215e55e2f54db

--- questTracker/sw.js
+++ questTracker/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.7858a6897d0d4e6e501b1cf08e979f09.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.3de82b8b316d8b9c1c8649b3e8025ede.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 workbox.skipWaiting();
 workbox.clientsClaim();
