fd98bb6bbdf562100a323918c3af1f97fb15ab45
taplar <taplarhw@gmail.com>
2019-02-23 14:50:17 -0600

    Added directory questTrackerDev and updated it to develop revision 6b68a92aff177866c4ec8622566222e0cbab9ac8

---
+++ questTrackerDev/precache-manifest.11988261f7e623ce6a000eb4877bf778.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4323816f338cc006a74a540fad4c1bc",
+    "url": "assets/styles/vendor/fontello/css/fontello-codes.css"
+  },
+  {
+    "revision": "83f449675127b3841149",
+    "url": "main.css"
+  },
+  {
+    "revision": "83f449675127b3841149",
+    "url": "main.js"
+  },
+  {
+    "revision": "b5d3e3b351e4e87a894f8c0a7ce006da",
+    "url": "assets/styles/vendor/fontello/css/fontello-embedded.css"
+  },
+  {
+    "revision": "eebbe0b838af5f76d3936f1bbf3de119",
+    "url": "assets/styles/vendor/fontello/README.txt"
+  },
+  {
+    "revision": "64dc4203fef6ea0bb7cdc9a5ff59dd43",
+    "url": "assets/styles/vendor/fontello/font/fontello.woff2"
+  },
+  {
+    "revision": "c8ee182ad01fb0120f26c5ced0f1bb1f",
+    "url": "assets/styles/vendor/fontello/font/fontello.woff"
+  },
+  {
+    "revision": "462d90b7c7c2316a7e273798c1cbffb0",
+    "url": "assets/styles/vendor/fontello/font/fontello.ttf"
+  },
+  {
+    "revision": "534be1241ad9da6d236fa8878e024f4a",
+    "url": "assets/styles/vendor/fontello/font/fontello.svg"
+  },
+  {
+    "revision": "25f7a6c366fab30b5f01cdce3bea1ceb",
+    "url": "assets/styles/vendor/fontello/font/fontello.eot"
+  },
+  {
+    "revision": "18e55f5502d52e35f665af6faa3e5ce0",
+    "url": "assets/styles/vendor/fontello/config.json"
+  },
+  {
+    "revision": "5efb6f925470166045ba28c25131f79a",
+    "url": "assets/styles/vendor/fontello/css/animation.css"
+  },
+  {
+    "revision": "552ba85815935179cdbf0151760d3c29",
+    "url": "index.html"
+  },
+  {
+    "revision": "69c26dc61b2ad85e226052e8ec944871",
+    "url": "assets/styles/vendor/fontello/css/fontello.css"
+  },
+  {
+    "revision": "d55a27e3a1c2756ffa87068418be0a4e",
+    "url": "assets/styles/vendor/fontello/css/fontello-ie7.css"
+  },
+  {
+    "revision": "b42fea219797f6700f635d334ee6cef9",
+    "url": "assets/styles/vendor/fontello/css/fontello-ie7-codes.css"
+  },
+  {
+    "revision": "c5571948164c09802c8aeab74c79b204",
+    "url": "assets/styles/vendor/fontello/LICENSE.txt"
+  },
+  {
+    "revision": "38d566aad27bf739a30b4ba9a40a039b",
+    "url": "assets/jsons/sagas.json"
+  },
+  {
+    "revision": "54832630aab7351c624e2af0ab724779",
+    "url": "assets/jsons/quests.json"
+  },
+  {
+    "revision": "eaf6021a8732bbf849986fa6954e9141",
+    "url": "assets/jsons/packs.json"
+  },
+  {
+    "revision": "a738dea46f81d443325b6fb3e80dd1d7",
+    "url": "assets/jsons/manifest.json"
+  },
+  {
+    "revision": "c513a592603129a61a147d3be8071c07",
+    "url": "assets/images/icons/d20Icon_512x512.png"
+  },
+  {
+    "revision": "f9affdd08c00e9874ade32ad5a72ccd3",
+    "url": "assets/images/icons/d20Icon_192x192.png"
+  },
+  {
+    "revision": "8cae760c593acd34166888fa5289c966",
+    "url": "assets/images/icons/d20Icon.svg"
+  }
+];
---
+++ questTrackerDev/sw.js
@@ -0,0 +1,248 @@
+importScripts("precache-manifest.11988261f7e623ce6a000eb4877bf778.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+( function () { //IIFE just for code collapsing
+	let currentCacheNames = Object.assign(
+		{ precacheTemp: workbox.core.cacheNames.precache + "-temp" }
+		, workbox.core.cacheNames
+	);
+
+	self.addEventListener( 'activate', function ( event ) {
+		event.waitUntil(
+			caches.keys().then( function ( cacheNames ) {
+				let validCacheSet = new Set( Object.values( currentCacheNames ) );
+
+				return Promise.all(
+					cacheNames.filter( function ( cacheName ) {
+						return !validCacheSet.has(cacheName);
+					} ).map( function ( cacheName ) {
+						return caches.delete( cacheName );
+					} )
+				);
+			})
+		);
+	});
+
+	self.__precacheManifest = [].concat(self.__precacheManifest || []);
+	workbox.precaching.suppressWarnings();
+	workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+	workbox.routing.registerRoute(/^http[s]?:\/\/fonts.googleapis.com\/(.*)/, workbox.strategies.staleWhileRevalidate(), 'GET');
+	workbox.routing.registerRoute(/^http[s]?:\/\/fonts.gstatic.com\/(.*)/, workbox.strategies.staleWhileRevalidate(), 'GET');
+	
+	workbox.routing.registerRoute(/\/assets\/styles\/vendor\/fontello\/font\/(.+)[.](.+)[?](.+)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+}() );
+
+( function () {
+	function DBCollection ( databasePromise, collectionName ) {
+		var self = this;
+		var collectionPromise = new Promise( function ( resolve, reject ) {
+			databasePromise.then(
+				function ( database ) {
+					if ( database.objectStoreNames.contains( collectionName ) ) {
+						resolve( database );
+					} else {
+						reject( 'collection.does.not.exist' );
+					}
+				}, reject
+			);
+		} );
+	
+		self.findAll = function () {
+			return new Promise( function ( resolve, reject ) {
+				collectionPromise.then(
+					function ( database ) {
+						var transaction = database.transaction( collectionName, IDBTransaction.READ_ONLY );
+						var store = transaction.objectStore( collectionName );
+						var request = store.getAll();
+	
+						request.onsuccess = function () {
+							resolve( request.result );
+						};
+	
+						request.onerror = function () {
+							reject( 'collection.operation.error' );
+						};
+					}, reject
+				);
+			} );
+		}
+	}
+	
+	function DBConnection ( databaseName, version, collections ) {
+		var self = this;
+		var dbPromise = new Promise( function ( resolve, reject ) {
+			if ( indexedDB ) {
+				var request = indexedDB.open( databaseName, version );
+	
+				request.onupgradeneeded = function () {
+					var database = request.result;
+	
+					if ( collections ) {
+						collections.forEach( function ( collection ) {
+							database.createObjectStore( collection.name, { keyPath: collection.id } );
+						} );
+					}
+				};
+	
+				request.onsuccess = function () {
+					resolve( request.result );
+				};
+	
+				request.onerror = function () {
+					reject( 'database.error' );
+				};
+			} else {
+				reject( 'database.not.supported' );
+			}
+		} );
+	
+		self.getCollection = function ( collectionName ) {
+			return new DBCollection( dbPromise, collectionName );
+		};
+	}
+	
+	let QuestTracker = ( function () {
+		var database;
+		var allPacksPromise;
+		var allQuestsPromise;
+		var allSagasPromise;
+	
+		var functions = {
+			retrieveJsonFromPreCache: function ( fileName ) {
+				return new Promise( function ( resolve, reject ) {
+					caches.open( workbox.core.cacheNames.precache ).then( function ( cache ) {
+						cache.keys().then( function ( cacheKeys ) {
+							var cacheKey = cacheKeys.find( function ( element ) {
+								return element.url.endsWith( fileName );
+							} );
+
+							cache.match( cacheKey ).then( function ( response ) {
+								response.json().then( function ( json ) {
+									resolve( json );
+								} );
+							} );
+						} );
+					} );
+				} );
+			}
+			, retrievePacks: function () {
+				if ( !allPacksPromise ) allPacksPromise = functions.retrieveJsonFromPreCache( 'packs.json' );
+				return allPacksPromise;
+			}
+			, retrieveQuests: function () {
+				if ( !allQuestsPromise ) allQuestsPromise = functions.retrieveJsonFromPreCache( 'quests.json' );
+				return allQuestsPromise;
+			}
+			, retrieveSagas: function () {
+				if ( !allSagasPromise ) allSagasPromise = functions.retrieveJsonFromPreCache( 'sagas.json' );
+				return allSagasPromise;
+			}
+			, respondWith: function ( data, status ) {
+				if ( status == 204 ) {
+					return new Response( null, { status: status } );
+				} else {
+					return new Response( JSON.stringify( data ), { status: status, headers: { 'Content-Type': 'application/json; charset=utf-8' } } );
+				}
+			}
+			, searchForQuests: function ( searchCriteria ) {
+				return new Promise( function ( resolve, reject ) {
+					Promise.all( [ functions.retrieveQuests(), functions.retrievePacks() ] ).then(
+						function ( values ) {
+							var quests = values[ 0 ];
+							var packs = values[ 1 ];
+
+							packs.forEach( function ( pack ) {
+								pack.quests.forEach( function ( id ) {
+									quests.filter( function ( quest ) {
+										return quest.id === id;
+									} )[ 0 ].pack = pack;
+								} );
+							} );
+
+							resolve( functions.respondWith( quests, 200 ) );
+						}
+						, function () {
+							reject( functions.respondWith( [], 500 ) );
+						}
+					);
+				} );
+			}
+			, searchForSagas: function ( searchCriteria ) {
+				return new Promise( function ( resolve, reject ) {
+					Promise.all( [ functions.retrieveSagas(), functions.retrievePacks() ] ).then(
+						function ( values ) {
+							var sagas = values[ 0 ];
+							var packs = values[ 1 ];
+
+							packs.filter( function ( pack ) {
+								return pack.sagas;
+							} ).forEach( function ( pack ) {
+								pack.sagas.forEach( function ( id ) {
+									sagas.filter( function ( saga ) {
+										return saga.id === id;
+									} )[ 0 ].pack = pack;
+								} );
+							} );
+
+							resolve( functions.respondWith( sagas, 200 ) );
+						}
+						, function () {
+							reject( functions.respondWith( [], 500 ) );
+						}
+					);
+				} );
+			}
+		};
+	
+		let api = {
+			initialize: function () {
+				return new Promise( function ( resolve, reject ) {
+					try {
+						database = new DBConnection( 'ddoQuestTracker', 1, [ { name: 'characters', id: 'id' } ] );
+
+						resolve( functions.respondWith( {}, 200 ) );
+					} catch ( e ) {
+						reject( functions.respondWith( {}, 500 ) );
+					}
+				} );
+			}
+			, registered: function () {
+				return new Promise( function ( resolve, reject ) {
+					resolve( functions.respondWith( {}, 200 ) );
+				} );
+			}
+			, retrieveCharacters: function () {
+				return new Promise( function ( resolve, reject ) {
+					database.getCollection( 'characters' ).findAll().then(
+						function ( characters ) {
+							let status = ( characters || [] ).length ? 200 : 204;
+
+							resolve( functions.respondWith( characters, status ) );
+						}
+						, function () {
+							reject( functions.respondWith( {}, 500 ) );
+						}
+					);
+				} );
+			}
+			, retrieveAllQuests: function ( event ) {
+				return functions.searchForQuests( {} );
+			}
+			, retrieveAllSagas: function ( event ) {
+				return functions.searchForSagas( {} );
+			}
+		};
+	
+		return api;
+	}() );
+
+	workbox.routing.registerRoute( /api\/initialize/, QuestTracker.initialize, 'GET' );
+	workbox.routing.registerRoute( /api\/registered/, QuestTracker.registered, 'HEAD' );
+
+	workbox.routing.registerRoute( /api\/quests$/, QuestTracker.retrieveAllQuests, 'GET' );
+	workbox.routing.registerRoute( /api\/sagas$/, QuestTracker.retrieveAllSagas, 'GET' );
+}() );
+
