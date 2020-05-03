8d97f6e464ab76b4dfd29fc7d8b5493c631311d9
taplar <taplarhw@gmail.com>
2020-05-03 13:39:19 -0500

    Updating to revision 07eaf5b97ad670276f1c361ac323a2b9ea4976e6

--- questTracker/precache-manifest.a70fb41ffd3f2aaa17bfa65715b259e3.js
+++ questTracker/precache-manifest.a70fb41ffd3f2aaa17bfa65715b259e3.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "b5d3e3b351e4e87a894f8c0a7ce006da",
-    "url": "assets/styles/vendor/fontello/css/fontello-embedded.css"
+    "revision": "d4323816f338cc006a74a540fad4c1bc",
+    "url": "assets/styles/vendor/fontello/css/fontello-codes.css"
   },
   {
-    "revision": "bfc1015e406422c7bc51",
+    "revision": "15eacd1eb797a6dfce83",
     "url": "main.css"
   },
   {
-    "revision": "bfc1015e406422c7bc51",
+    "revision": "15eacd1eb797a6dfce83",
     "url": "main.js"
   },
   {
-    "revision": "b42fea219797f6700f635d334ee6cef9",
-    "url": "assets/styles/vendor/fontello/css/fontello-ie7-codes.css"
+    "revision": "b5d3e3b351e4e87a894f8c0a7ce006da",
+    "url": "assets/styles/vendor/fontello/css/fontello-embedded.css"
   },
   {
     "revision": "eebbe0b838af5f76d3936f1bbf3de119",
@@ -24,44 +24,44 @@ self.__precacheManifest = [
     "url": "assets/styles/vendor/fontello/css/fontello.css"
   },
   {
-    "revision": "534be1241ad9da6d236fa8878e024f4a",
-    "url": "assets/styles/vendor/fontello/font/fontello.svg"
-  },
-  {
     "revision": "64dc4203fef6ea0bb7cdc9a5ff59dd43",
     "url": "assets/styles/vendor/fontello/font/fontello.woff2"
-  },
-  {
-    "revision": "462d90b7c7c2316a7e273798c1cbffb0",
-    "url": "assets/styles/vendor/fontello/font/fontello.ttf"
   },
   {
     "revision": "c8ee182ad01fb0120f26c5ced0f1bb1f",
     "url": "assets/styles/vendor/fontello/font/fontello.woff"
   },
   {
-    "revision": "5efb6f925470166045ba28c25131f79a",
-    "url": "assets/styles/vendor/fontello/css/animation.css"
+    "revision": "462d90b7c7c2316a7e273798c1cbffb0",
+    "url": "assets/styles/vendor/fontello/font/fontello.ttf"
   },
   {
-    "revision": "d4323816f338cc006a74a540fad4c1bc",
-    "url": "assets/styles/vendor/fontello/css/fontello-codes.css"
-  },
-  {
-    "revision": "552ba85815935179cdbf0151760d3c29",
-    "url": "index.html"
+    "revision": "534be1241ad9da6d236fa8878e024f4a",
+    "url": "assets/styles/vendor/fontello/font/fontello.svg"
   },
   {
     "revision": "25f7a6c366fab30b5f01cdce3bea1ceb",
     "url": "assets/styles/vendor/fontello/font/fontello.eot"
   },
   {
+    "revision": "18e55f5502d52e35f665af6faa3e5ce0",
+    "url": "assets/styles/vendor/fontello/config.json"
+  },
+  {
+    "revision": "add3ecae3fc5768c3a9bed131dffa1e8",
+    "url": "index.html"
+  },
+  {
+    "revision": "5efb6f925470166045ba28c25131f79a",
+    "url": "assets/styles/vendor/fontello/css/animation.css"
+  },
+  {
     "revision": "d55a27e3a1c2756ffa87068418be0a4e",
     "url": "assets/styles/vendor/fontello/css/fontello-ie7.css"
   },
   {
-    "revision": "18e55f5502d52e35f665af6faa3e5ce0",
-    "url": "assets/styles/vendor/fontello/config.json"
+    "revision": "b42fea219797f6700f635d334ee6cef9",
+    "url": "assets/styles/vendor/fontello/css/fontello-ie7-codes.css"
   },
   {
     "revision": "c5571948164c09802c8aeab74c79b204",
@@ -72,16 +72,20 @@ self.__precacheManifest = [
     "url": "assets/jsons/sagas.json"
   },
   {
-    "revision": "e55fe6dff1402f6c4d85370e3c5e4c22",
+    "revision": "7e9bde2ca6a7df7e54895476011a0881",
     "url": "assets/jsons/quests.json"
   },
   {
-    "revision": "df68732f03053403caef926daa5b8956",
+    "revision": "08ad9f5cec177d7eea0a56e17f50589f",
     "url": "assets/jsons/packs.json"
   },
   {
     "revision": "a738dea46f81d443325b6fb3e80dd1d7",
     "url": "assets/jsons/manifest.json"
+  },
+  {
+    "revision": "6d2533818efede5060d4542922b80ba8",
+    "url": "assets/jsons/manageQuests.html"
   },
   {
     "revision": "c513a592603129a61a147d3be8071c07",
--- questTracker/sw.js
+++ questTracker/sw.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.39713e522b23826de44a931d91f0420d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("precache-manifest.a70fb41ffd3f2aaa17bfa65715b259e3.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 'use strict';
 
